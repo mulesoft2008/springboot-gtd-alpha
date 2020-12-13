151bf1594d6fbf5ea480c718f17ff231ee65aee8
yibeltal <mulesoft2008@gmail.com>
2020-12-13 17:05:42 -0500

    changed the greeting into sidamigna

--- src/main/java/edu/lab/demo/GreetingResource.java
+++ src/main/java/edu/lab/demo/GreetingResource.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingResource {
 
-	private static final String template = "Hola comostas, %s!";
+	private static final String template = "Kero galito Magano galatamo, %s!";
     private final AtomicLong counter = new AtomicLong(); // stateful counter...
 
     @GetMapping("/api/greetings/greeting")
